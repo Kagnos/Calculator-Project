d8cd1b8ed42dd995559604101719434dd4d156cc
Kagnos <kagcodes@gmail.com>
2026-01-27 14:04:47 -0600

    Javascript functions and styling Horizontally center display box text Defer script in html script tag Create math operation functions Create operate function that calls math operation functions Create variables that reference html boxes and buttons Give number buttons functionality to show in display box

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Page loaded and script running!");
+const add = (num1, num2) => num1 + num2
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case add:
+            return add(num1, num2);
+        case subtract:
+            return subtract(num1, num2);
+        case multiply:
+            return multiply(num1, num2);
+        case divide:
+            return divide(num1, num2);
+    }
+}
+
+const displayBox = document.querySelector("#display-box");
+const errorBox = document.querySelector("#error-box");
+const clearButton = document.querySelector("#clear-button");
+const deleteButton = document.querySelector("#delete-button");
+const sevenButton = document.querySelector("#seven-button");
+const eightButton = document.querySelector("#eight-button");
+const nineButton = document.querySelector("#nine-button");
+const divisionButton = document.querySelector("#division-button");
+const fourButton = document.querySelector("#four-button");
+const fiveButton = document.querySelector("#five-button");
+const sixButton = document.querySelector("#six-button");
+const multiplicationButton = document.querySelector("#multiplication-button");
+const oneButton = document.querySelector("#one-button");
+const twoButton = document.querySelector("#two-button");
+const threeButton = document.querySelector("#three-button");
+const additionButton = document.querySelector("#addition-button");
+const zeroButton = document.querySelector("#zero-button");
+const decimalButton = document.querySelector("#decimal-button");
+const equalityButton = document.querySelector("#equality-button");
+const subtractionButton = document.querySelector("#subtraction-button");
+
+sevenButton.addEventListener("click", (event) => {
+    displayBox.textContent = "7";
+});
+
+eightButton.addEventListener("click", (event) => {
+    displayBox.textContent = "8";
+});
+
+nineButton.addEventListener("click", (event) => {
+    displayBox.textContent = "9";
+});
+
+fourButton.addEventListener("click", (event) => {
+    displayBox.textContent = "4";
+});
+
+fiveButton.addEventListener("click", (event) => {
+    displayBox.textContent = "5";
+});
+
+sixButton.addEventListener("click", (event) => {
+    displayBox.textContent = "6";
+});
+
+oneButton.addEventListener("click", (event) => {
+    displayBox.textContent = "1";
+});
+
+twoButton.addEventListener("click", (event) => {
+    displayBox.textContent = "2";
+});
+
+threeButton.addEventListener("click", (event) => {
+    displayBox.textContent = "3";
+});
+
+zeroButton.addEventListener("click", (event) => {
+    displayBox.textContent = "0";
 });
