ab85cd0a7b68fc8d951379e72ef75ac405cb738d
Kagnos <kagcodes@gmail.com>
2026-01-30 14:05:29 -0600

    Update button functionalities and optimize code Change decimal point cut off from two decimal points to three Update decimal button functionality, adding a decimal after an operator adds "0." instead of doing nothing Fix bug where user can input multiple decimals per number Fix bug where negative button would change "0." and "0.0..." to 0 Clean up JavaScript code with headers and functions for button event listeners

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// Global Variables
+
+const displayBox = document.querySelector("#display-box");
+const clearButton = document.querySelector("#clear-button");
+const modulusButton = document.querySelector("#modulus-button");
+const divisionButton = document.querySelector("#division-button");
+const deleteButton = document.querySelector("#delete-button");
+const sevenButton = document.querySelector("#seven-button");
+const eightButton = document.querySelector("#eight-button");
+const nineButton = document.querySelector("#nine-button");
+const multiplicationButton = document.querySelector("#multiplication-button");
+const fourButton = document.querySelector("#four-button");
+const fiveButton = document.querySelector("#five-button");
+const sixButton = document.querySelector("#six-button");
+const additionButton = document.querySelector("#addition-button");
+const oneButton = document.querySelector("#one-button");
+const twoButton = document.querySelector("#two-button");
+const threeButton = document.querySelector("#three-button");
+const subtractionButton = document.querySelector("#subtraction-button");
+const negativeButton = document.querySelector("#negative-button");
+const zeroButton = document.querySelector("#zero-button");
+const decimalButton = document.querySelector("#decimal-button");
+const equalityButton = document.querySelector("#equality-button");
+let equation = displayBox.textContent.split(" ");
+
+// Calculator Functions
+
 const add = (num1, num2) => num1 + num2
 
 const subtract = (num1, num2) => num1 - num2;
@@ -32,8 +59,8 @@ function solveEquation() {
         displayBox.textContent = "Err"
     } else if (solution.toString().includes(".") === true){
         const solutionArray = solution.toString().split(".")
-        if (solutionArray[1].length >= 2) {
-            displayBox.textContent = solution.toFixed(2);
+        if (solutionArray[1].length > 3) {
+            displayBox.textContent = solution.toFixed(3);
         } else {
             displayBox.textContent = solution;
         }
@@ -42,310 +69,199 @@ function solveEquation() {
     }
 }
 
-const displayBox = document.querySelector("#display-box");
-const clearButton = document.querySelector("#clear-button");
-const modulusButton = document.querySelector("#modulus-button");
-const divisionButton = document.querySelector("#division-button");
-const deleteButton = document.querySelector("#delete-button");
-const sevenButton = document.querySelector("#seven-button");
-const eightButton = document.querySelector("#eight-button");
-const nineButton = document.querySelector("#nine-button");
-const multiplicationButton = document.querySelector("#multiplication-button");
-const fourButton = document.querySelector("#four-button");
-const fiveButton = document.querySelector("#five-button");
-const sixButton = document.querySelector("#six-button");
-const additionButton = document.querySelector("#addition-button");
-const oneButton = document.querySelector("#one-button");
-const twoButton = document.querySelector("#two-button");
-const threeButton = document.querySelector("#three-button");
-const subtractionButton = document.querySelector("#subtraction-button");
-const negativeButton = document.querySelector("#negative-button");
-const zeroButton = document.querySelector("#zero-button");
-const decimalButton = document.querySelector("#decimal-button");
-const equalityButton = document.querySelector("#equality-button");
-
-let equation = displayBox.textContent.split(" ");
-
-clearButton.addEventListener("click", (event) => {
-    displayBox.textContent = "0";
+function updateEquationVariable() {
     equation = displayBox.textContent.split(" ");
-})
+};
 
-deleteButton.addEventListener("click", (event) => {
+// Button Functions
+
+function pressClearButton() {
+    displayBox.textContent = "0";
+    updateEquationVariable();
+};
+
+function pressDeleteButton() {
     if (equation.length === 1 && equation[0].length === 1 || equation[equation.length - 1] === "Err") {
         displayBox.textContent = "0";
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     } else if (equation[equation.length - 1] === "") {
         displayBox.textContent = displayBox.textContent.trim().slice(0,-1).trim();
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     } else {
         displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     }
-})
+}
 
-modulusButton.addEventListener("click", (event) => {
+function pressNumberButton(num) {
+    if (displayBox.textContent.length >= 15) {
+        return;
+    } else if (equation[equation.length - 1] === "Err") {
+        displayBox.textContent = num;
+        updateEquationVariable();
+    } else if (equation[equation.length - 1] === "0") {
+        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
+        displayBox.textContent += num;
+        updateEquationVariable();
+    } else {
+        displayBox.textContent += num;
+        updateEquationVariable();
+    }
+};
+
+function pressOperatorButton(operator) {
     if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || displayBox.textContent.slice(-1) === " " || equation[equation.length - 1] === "Err") {
         return;
     } else if (equation.length === 3) {
         solveEquation();
-        displayBox.textContent += " % ";
-        equation = displayBox.textContent.split(" ");
+        displayBox.textContent += operator;
+        updateEquationVariable();
     } else {
-        displayBox.textContent += " % ";
-        equation = displayBox.textContent.split(" ");
+        displayBox.textContent += operator;
+        updateEquationVariable();
     }
-});
+};
 
-divisionButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || displayBox.textContent.slice(-1) === " " || equation[equation.length - 1] === "Err") {
-        return;
-    } else if (equation.length === 3) {
-        solveEquation();
-        displayBox.textContent += " / ";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += " / ";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-sevenButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "7";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "7";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "7";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-eightButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "8";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "8";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "8";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-nineButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "9";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "9";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "9";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-multiplicationButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || displayBox.textContent.slice(-1) === " " || equation[equation.length - 1] === "Err") {
-        return;
-    } else if (equation.length === 3) {
-        solveEquation();
-        displayBox.textContent += " * ";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += " * ";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-fourButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "4";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "4";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "4";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-fiveButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "5";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "5";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "5";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-sixButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "6";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "6";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "6";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-additionButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || displayBox.textContent.slice(-1) === " " || equation[equation.length - 1] === "Err") {
-        return;
-    } else if (equation.length === 3) {
-        solveEquation();
-        displayBox.textContent += " + ";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += " + ";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-oneButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "1";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "1";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "1";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-twoButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "2";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "2";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "2";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-threeButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15) {
-        return;
-    } else if (equation[equation.length - 1] === "Err") {
-        displayBox.textContent = "3";
-        equation = displayBox.textContent.split(" ");
-    } else if (equation[equation.length - 1] === "0") {
-        displayBox.textContent = displayBox.textContent.trim().slice(0,-1);
-        displayBox.textContent += "3";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += "3";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-subtractionButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || displayBox.textContent.slice(-1) === " " || equation[equation.length - 1] === "Err") {
-        return;
-    } else if (equation.length === 3) {
-        solveEquation();
-        displayBox.textContent += " - ";
-        equation = displayBox.textContent.split(" ");
-    } else {
-        displayBox.textContent += " - ";
-        equation = displayBox.textContent.split(" ");
-    }
-});
-
-negativeButton.addEventListener("click", (event) => {
-    if (displayBox.textContent.length >= 15 || equation[equation.length - 1] === "" || equation[equation.length - 1] === "Err") {
+function pressNegativeButton() {
+    if (displayBox.textContent.length >= 15 || equation[equation.length - 1] === "" || equation[equation.length - 1] === "Err" || parseInt(equation[equation.length - 1]) === 0) {
         return;
     } else {
         equation[equation.length - 1] = String(equation[equation.length - 1] * -1) 
         displayBox.textContent = equation;
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     }
-});
+};
 
-zeroButton.addEventListener("click", (event) => {
+function pressZeroButton() {
     if (displayBox.textContent.length >= 15 || (equation[equation.length - 1] === "0")) {
         return;
     } else if (equation[equation.length - 1] === "Err") {
         displayBox.textContent = "0";
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     } else {
         displayBox.textContent += "0";
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     }
-});
+}
 
-decimalButton.addEventListener("click", (event) => {  
-    if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || displayBox.textContent.slice(-1) === "." || displayBox.textContent.slice(-1) === " ") {
+function pressDecimalButton() {
+        if (displayBox.textContent.length >= 15 || displayBox.textContent === "" || equation[equation.length - 1].includes(".") === true) {
         return;
     } else if (equation[equation.length - 1] === "Err") {
         displayBox.textContent = "0.";
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
+    } else if (displayBox.textContent.slice(-1) === " ") {
+        displayBox.textContent += "0.";
+        updateEquationVariable();
     } else {
         displayBox.textContent += ".";
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     }
-});
+};
 
-equalityButton.addEventListener("click", (event) => {
+function pressEqualityButton() {
     if (equation[2] !== "" && equation[2] !== undefined) {
         solveEquation();
-        equation = displayBox.textContent.split(" ");
+        updateEquationVariable();
     } else {
         return;
     }
+};
+
+// Button Event Listeners
+
+clearButton.addEventListener("click", (event) => {
+    pressClearButton();
+});
+
+deleteButton.addEventListener("click", (event) => {
+    pressDeleteButton();
+});
+
+modulusButton.addEventListener("click", (event) => {
+    pressOperatorButton(" % ");
+});
+
+divisionButton.addEventListener("click", (event) => {
+    pressOperatorButton(" / ");
+});
+
+sevenButton.addEventListener("click", (event) => {
+    pressNumberButton("7");
+});
+
+eightButton.addEventListener("click", (event) => {
+    pressNumberButton("8");
+});
+
+nineButton.addEventListener("click", (event) => {
+    pressNumberButton("9");
+});
+
+multiplicationButton.addEventListener("click", (event) => {
+    pressOperatorButton(" * ");
+});
+
+fourButton.addEventListener("click", (event) => {
+    pressNumberButton("4");
+});
+
+fiveButton.addEventListener("click", (event) => {
+    pressNumberButton("5");
+});
+
+sixButton.addEventListener("click", (event) => {
+    pressNumberButton("6");
+});
+
+additionButton.addEventListener("click", (event) => {
+    pressOperatorButton(" + ");
+});
+
+oneButton.addEventListener("click", (event) => {
+    pressNumberButton("1");
+});
+
+twoButton.addEventListener("click", (event) => {
+    pressNumberButton("2");
+});
+
+threeButton.addEventListener("click", (event) => {
+    pressNumberButton("3");
+});
+
+subtractionButton.addEventListener("click", (event) => {
+    pressOperatorButton(" - ");
+});
+
+negativeButton.addEventListener("click", (event) => {
+    pressNegativeButton();
+});
+
+zeroButton.addEventListener("click", (event) => {
+    pressZeroButton();
+});
+
+decimalButton.addEventListener("click", (event) => {  
+    pressDecimalButton();
+});
+
+equalityButton.addEventListener("click", (event) => {
+    pressEqualityButton();
 });
 
 // Keep 0 when user inputs an operator - DONE
 // Make default screen 0 - DONE
 // add modulus button functionality - DONE
 // add error message for divide by zero - DONE
+// clear error message when user inputs with a button - DONE
+// Replace 0 when user inputs a number - DONE
+// add negative button functionality - DONE
+// Restrict user to only input one 0 at the beginning of each number - DONE
+// fix bug: can use multiple decimals in a number - DONE
 
-// fix bug: decimals not tracking
-// fix bug: can use multiple decimals in a number
-// clear error message when user inputs with a button
-// add negative button functionality
-// Replace 0 when user inputs a number
-// Restrict user to only input one 0 at the beginning of each number
+// fix bug: decimals not tracking, everything always gets rounded down for some reason except with division for some reason
+// fix bug: negative button breaks second number
+// replace solution with number if number button pressed after equality button pressed
 // Change favicon to be red, orange, gray, green or some other combination that includes the orange color
 // Allow user to use keyboard for inputs
